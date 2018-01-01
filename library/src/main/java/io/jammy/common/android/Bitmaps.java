9874a1caa04f7caa2da922e3ce8ca266d9f6e444
Matt Fenlon <nolnefm@gmail.com>
2018-01-01 08:35:19 -0800

    Added method to convert VectorDrawable to Bitmap

--- library/src/main/java/io/jammy/common/android/Bitmaps.java
+++ library/src/main/java/io/jammy/common/android/Bitmaps.java
@@ -2,8 +2,13 @@ package io.jammy.common.android;
 
 import static android.content.ContentValues.TAG;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.VectorDrawable;
+import android.os.Build;
+import android.support.graphics.drawable.VectorDrawableCompat;
 import android.util.Log;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -53,5 +58,26 @@ public class Bitmaps {
     Log.i(TAG, String.format(Locale.getDefault(), "File -> %s", file));
 
     return file;
+  }
+
+  @TargetApi (Build.VERSION_CODES.LOLLIPOP)
+  public static Bitmap getBitmap(VectorDrawable vectorDrawable) {
+
+    Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+    Canvas canvas = new Canvas(bitmap);
+    vectorDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+    vectorDrawable.draw(canvas);
+    
+    return bitmap;
+  }
+
+  public static Bitmap getBitmap(VectorDrawableCompat vectorDrawable) {
+
+    Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+    Canvas canvas = new Canvas(bitmap);
+    vectorDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+    vectorDrawable.draw(canvas);
+
+    return bitmap;
   }
 }
